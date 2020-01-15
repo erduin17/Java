ab0f08896982be712f89dc5c913d4c84e9cce963
erduin17 <erduinmacias.cs@gmail.com>
2020-01-15 12:47:33 -0800

    first commit

---
+++ WhatIsToday.java
@@ -0,0 +1,22 @@
+//Name: Erduin Macias
+//Date: Jan 15 2020
+//File: WhatIsToday.java
+
+public class WhatIsToday {
+
+   public static void main (String[] args){
+      //declare variables
+      int month;
+      int day;
+      
+      //set variables
+      month = 1;
+      day = 10;
+      
+      //print output
+      System.out.println("The month is January");
+      System.out.println("January is month number " + month);
+      System.out.println("The day of the month is: " + day);
+      
+   }
+}
